1d07134e4ad75a884f61776f25a0c6759b7a2534
Bruna <brunaalmeida075@gmail.com>
2022-11-22 18:11:57 -0300

    teste 3 ok

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Verificando se aparece na tela"No favorite pokemon found"', () => {
+  renderWithRouter(<App />);
+
+  const Link = screen.getByRole('link', { name: /Favorite Pokémon/i });
+  userEvent.click(Link);
+
+  const naoFavorito = screen.getByText(/No favorite Pokémon found/i);
+
+  expect(naoFavorito).toBeInTheDocument();
+});
