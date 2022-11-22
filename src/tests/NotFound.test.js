bf845253563009d5547f68b123dc89a3d80f2a23
Bruna <brunaalmeida075@gmail.com>
2022-11-22 18:22:18 -0300

    teste 4 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const urlInvalida = 'details';
+
+  act(() => history.push(urlInvalida));
+
+  const pagNaoEncontrada = screen.getByRole('heading', {
+    name: /Page requested not found/i,
+    level: 2 });
+
+  const img = screen.getByRole('img', {
+    src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  });
+
+  expect(pagNaoEncontrada).toBeInTheDocument();
+  expect(img).toBeInTheDocument();
+});
