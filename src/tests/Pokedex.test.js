3a1ea9283a0546a9b84e4325e492c0b852d67188
Bruna <brunaalmeida075@gmail.com>
2022-11-23 17:33:30 -0300

    requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,19 +1,44 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
+import data from '../data';
 import App from '../App';
 
-test('Verificando se ha um titulo', () => {
-  renderWithRouter(<App />);
+describe('verificando page de Pokedex', () => {
+  test('Verificando se ha um titulo', () => {
+    renderWithRouter(<App />);
 
-  const titulo = screen.getByRole('heading', {
-    name: /Encountered Pokémon/i,
-    level: 2,
+    const titulo = screen.getByRole('heading', {
+      name: /Encountered Pokémon/i,
+      level: 2,
+    });
+    expect(titulo).toBeInTheDocument();
   });
-  expect(titulo).toBeInTheDocument();
-});
-test('', () => {
-  renderWithRouter(<App />);
+  test('Testando se o botão funciona e se ao clicar é exibido o próximo Pokémon', () => {
+    renderWithRouter(<App />);
 
-  const botao = screen.getByRole('button', { name: /próximo pokémon/i });
-  expect(botao).toBeInTheDocument();
+    const botao = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(botao).toBeInTheDocument();
+
+    data.forEach((pokemon, index) => {
+      userEvent.click(botao);
+      const nomePoke = pokemon.name;
+      expect(nomePoke).toBe(data[index].name);
+    });
+  });
+  test('Verificando se o Pokedex tem o botão de filtro', () => {
+    renderWithRouter(<App />);
+    const button = screen.getAllByTestId('pokemon-type-button');
+    button.forEach((e) => {
+      const btn = e.innerHTML;
+      expect(btn).toBe(e.innerHTML);
+    });
+  });
+  test('Testando botão TUDO', () => {
+    renderWithRouter(<App />);
+    const btnTudo = screen.getByRole('button', { name: /all/i });
+    userEvent.click(btnTudo);
+    expect(btnTudo).toBeInTheDocument();
+    expect(btnTudo.innerHTML).toBe('All');
+  });
 });
