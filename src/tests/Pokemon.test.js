ab4e186a5c53c7ea9dc840355d423d9057c10899
Bruna <brunaalmeida075@gmail.com>
2022-11-29 04:55:28 -0300

    teste 6 ok

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('', () => {
+  test('Testando se mostra as informaçoes do pokemon na tela', () => {
+    renderWithRouter(<App />);
+    const img = screen.getByRole('img', {
+      name: /pikachu sprite/i,
+      src: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    });
+    const nome = screen.getByTestId('pokemon-name');
+    const tipo = screen.getByTestId('pokemon-type');
+    const peso = screen.getByTestId('pokemon-weight');
+
+    expect(peso).toBeInTheDocument();
+    expect(tipo).toBeInTheDocument();
+    expect(nome).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+  test('Testando se tem o link de navegação no card', () => {
+    renderWithRouter(<App />);
+    const linkDetalhes = screen.getByRole('link', { name: /More details/i, href: '/pokemon/25' });
+    expect(linkDetalhes).toBeInTheDocument();
+
+    userEvent.click(linkDetalhes);
+
+    const detalhes = screen.getByRole('heading', { name: /pikachu details/i });
+    expect(detalhes).toBeInTheDocument();
+    expect(linkDetalhes).toHaveAttribute('href', '/pokemon/25');
+
+    const pokeNome = screen.getByTestId('pokemon-name');
+    expect(pokeNome).toBeInTheDocument();
+  });
+  test('Verificando se tem a img de favorito', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkDetalhes = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkDetalhes);
+    expect(linkDetalhes.href).toMatch(history.location.pathname);
+
+    const checkFavorito = await screen.findByRole('checkbox');
+    userEvent.click(checkFavorito);
+
+    const favorito = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(favorito).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+import App from '../App';
+
+describe('', () => {
+  test('', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+    const linkDetalhes = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkDetalhes);
+    // console.log(history);
+    const titulo = screen.getByRole('heading', { name: /pikachu details/i, level: 2 });
+    expect(titulo).toBeInTheDocument();
+    expect(linkDetalhes).not.toBeInTheDocument();
+    const SubTitulo = screen.getByRole('heading', { name: /summary/i, level: 2 });
+    expect(SubTitulo).toBeInTheDocument();
+    const nomeHtml = screen.getByTestId('pokemon-name');
+    const nome = nomeHtml.innerHTML;
+    //const [, pokemonId] = history.location.way(/ pokémon \/ ( \d + ) /);
+    // const pokemon = pokemonList.find(({ id }) => id === Number());
+    // expect(pokemon).toBe('o');
+  });
+});
